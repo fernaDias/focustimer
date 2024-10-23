fb1c670e0f4191050780cdf8eaac6b90ccdc03ae
Fernanda Dias <fernanda.dias.silva@accenture.com>
2024-10-23 09:59:47 -0300

    add counter on seconds

--- js/script.js
+++ js/script.js
@@ -7,12 +7,30 @@ const buttonStop = document.querySelector('.stop');
 const buttonSet = document.querySelector('.set');
 const buttonSoundOn =  document.querySelector('.sound-on');
 const buttonSoundOff =  document.querySelector('.sound-off');
+let minutes 
+const minutesDisplay = document.querySelector('.minutes');
+const secondsDisplay = document.querySelector('.seconds');
+
+function countDown() {
+  setTimeout(function() {
+    let seconds = Number(secondsDisplay.textContent)
+
+    if( seconds <= 0 ) {
+      seconds = 60
+    }
+    secondsDisplay.textContent = seconds-1
+    countDown()
+  }, 1000)
+}
 
 buttonPlay.addEventListener('click', function () {
   buttonPlay.classList.add('hide')  
   buttonPause.classList.remove('hide')
   buttonSet.classList.add('hide')
   buttonStop.classList.remove('hide')
+
+  countDown()
+
 })
 
 buttonPause.addEventListener('click', function () {
@@ -35,4 +53,8 @@ buttonSoundOff.addEventListener('click', function () {
 buttonSoundOn.addEventListener('click', function () {
   buttonSoundOff.classList.remove('hide')
   buttonSoundOn.classList.add('hide')
+})
+buttonSet.addEventListener('click', function () {
+  minutes = prompt('Quantos minutos?')
+  minutesDisplay.textContent = minutes
 })
