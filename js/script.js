90e478c20aeba421e8247b71f0cdd2c7d6787d3c
Fernanda Dias <fernanda.dias.silva@accenture.com>
2024-10-23 10:46:08 -0300

    work on logic to decrement time

--- js/script.js
+++ js/script.js
@@ -14,11 +14,24 @@ const secondsDisplay = document.querySelector('.seconds');
 function countDown() {
   setTimeout(function() {
     let seconds = Number(secondsDisplay.textContent)
+    let minutes = Number(minutesDisplay.textContent)
+
+    secondsDisplay.textContent = String(seconds-1).padStart(2, '0')
+
+    if ( minutes <= 0 ) {
+      buttonPlay.classList.remove('hide')
+      buttonPause.classList.add('hide')
+      buttonSet.classList.remove('hide')
+      buttonStop.classList.add('hide')
+
+      return
+    }
 
     if( seconds <= 0 ) {
       seconds = 60
+      minutesDisplay.textContent = String(minutes - 1).padStart(2, '0')
     }
-    secondsDisplay.textContent = seconds-1
+
     countDown()
   }, 1000)
 }
@@ -56,5 +69,5 @@ buttonSoundOn.addEventListener('click', function () {
 })
 buttonSet.addEventListener('click', function () {
   minutes = prompt('Quantos minutos?')
-  minutesDisplay.textContent = minutes
+  minutesDisplay.textContent = String(minutes).padStart(2, '0')
 })
