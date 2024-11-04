91cda1c1f93035a067e7ecaae386a1c499f2041a
Fernanda Dias <fernanda.dias.silva@accenture.com>
2024-11-04 20:17:11 -0300

    create events

--- js/index.js
+++ js/index.js
@@ -1,13 +1,12 @@
 import Controls from "./controls.js";
 import Timer from "./timer.js";
-import Sound from "./sounds.js";
+import Sound from "./sounds.js"
+import Events from "./events.js";
 import {
   buttonPause,
   buttonPlay,
   buttonSet,
   buttonStop,
-  buttonSoundOff,
-  buttonSoundOn,
   minutesDisplay,
   secondsDisplay,
 } from "./elements.js";
@@ -27,43 +26,5 @@ const timer = Timer({
 
 const sound = Sound();
 
-buttonPlay.addEventListener("click", function () {
-  controls.play();
-  timer.countDown();
-  sound.pressButton();
-});
+Events({controls, sound, timer});
 
-buttonPause.addEventListener("click", function () {
-  controls.pause();
-  timer.hold();
-  sound.pressButton();
-});
-
-buttonStop.addEventListener("click", function () {
-  controls.reset();
-  timer.reset();
-  sound.pressButton();
-});
-
-buttonSoundOff.addEventListener("click", function () {
-  buttonSoundOn.classList.remove("hide");
-  buttonSoundOff.classList.add("hide");
-  sound.bgAudio.pause();
-});
-
-buttonSoundOn.addEventListener("click", function () {
-  buttonSoundOff.classList.remove("hide");
-  buttonSoundOn.classList.add("hide");
-  sound.bgAudio.play();
-});
-
-buttonSet.addEventListener("click", function () {
-  let newMinutes = controls.getMinutes();
-
-  if (!newMinutes) {
-    timer.reset();
-    return;
-  }
-  timer.updateDisplay(newMinutes, 0);
-  timer.updateMinutes(newMinutes);
-});
